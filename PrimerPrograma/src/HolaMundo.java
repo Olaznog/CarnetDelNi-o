f73f1ad708ca84674d83910b6532f64195d13127
Gonzalo <Gonzalo@nb-gonzalo>
2017-07-02 12:08:27 -0300

    Prueba primer commit

---
+++ PrimerPrograma/src/HolaMundo.java
@@ -0,0 +1,10 @@
+
+public class HolaMundo {
+	public static void main (String [] args) {
+		/*
+		 * A continuación se despliega el mensaje que deseamos mostrar en pantalla
+		 */
+		System.out.println ("Puto el que lee!");
+	}
+
+}
