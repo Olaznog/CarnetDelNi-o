0d51af89ac36f44450330f611439d62fdd91e4d3
Feli <Feli@DESKTOP-IGL9CQI>
2017-07-15 11:24:30 -0300

    Agrego capa gráfica

---
+++ CarnetDelNino/src/grafica/VentanaConfiguracion.java
@@ -0,0 +1,67 @@
+package grafica;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class VentanaConfiguracion extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textCantMaxNiños;
+	private JTextField textCantMaxRegNiños;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaConfiguracion frame = new VentanaConfiguracion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaConfiguracion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 225);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCantMaxNiños = new JLabel("Ingrese la cantidad m\u00E1xima de ni\u00F1os:");
+		lblCantMaxNiños.setBounds(10, 91, 186, 22);
+		contentPane.add(lblCantMaxNiños);
+		
+		JLabel lblCantMaxRegNiños = new JLabel("Ingrese la cantidad m\u00E1xima de registros por ni\u00F1o:");
+		lblCantMaxRegNiños.setBounds(10, 124, 237, 22);
+		contentPane.add(lblCantMaxRegNiños);
+		
+		textCantMaxNiños = new JTextField();
+		textCantMaxNiños.setBounds(247, 92, 86, 20);
+		contentPane.add(textCantMaxNiños);
+		textCantMaxNiños.setColumns(10);
+		
+		textCantMaxRegNiños = new JTextField();
+		textCantMaxRegNiños.setBounds(247, 125, 86, 20);
+		contentPane.add(textCantMaxRegNiños);
+		textCantMaxRegNiños.setColumns(10);
+		
+		JLabel lblConfiguracinDeSistema = new JLabel("CONFIGURACI\u00D3N DE SISTEMA");
+		lblConfiguracinDeSistema.setBounds(120, 11, 148, 44);
+		contentPane.add(lblConfiguracinDeSistema);
+	}
+}
