493fad4b9edaaa57c07da4e7a4c9fd573c8c7e82
Gonzalo <Gonzalo@nb-gonzalo>
2017-07-29 12:51:33 -0300

    persistencia

--- CarnetDelNino/src/persistencia/RecuperarNiño.java
+++ CarnetDelNino/src/persistencia/RecuperarNiño.java
@@ -7,24 +7,24 @@ import java.io.ObjectInputStream;
 import logica.ColeccionNiños;
 
 public class RecuperarNiño {
-	public static void recuperar(String[] args) {
-		// TODO Auto-generated method stub
+	public static void recuperar() {
+		
 		FileInputStream fis;
 		try {			
-			fis = new FileInputStream("C://DatosNiño/datos.txt");
+			 fis = new FileInputStream("C://DatosNiño/datos.txt");
 			 ObjectInputStream ois = new ObjectInputStream(fis);
 			 ColeccionNiños niño = (ColeccionNiños) ois.readObject();
 			 ois.close();
 			 niño.listarNiños();
 			 fis.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 
